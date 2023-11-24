62cfcc8e987b12fe4096683275541256ba8b913e
bilal abdelkadir <bilalbinabdelkadir@gmail.com>
2023-11-24 23:53:11 +0300

    feat: added mail module

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly mailService: MailService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('send-email')
+  async sendMail() {
+    await this.mailService.testEmail();
+    return 'Email has been sent';
   }
 }
