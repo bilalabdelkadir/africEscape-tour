97c943b70a5c4a1d9c6484af508eeef63ae7d993
bilal abdelkadir <bilalbinabdelkadir@gmail.com>
2023-12-01 17:06:45 +0300

    feat: added initial setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper>
+      <h1 className="text-4xl font-bold">Home</h1>
+    </MaxWidthWrapper>
+  );
+}
