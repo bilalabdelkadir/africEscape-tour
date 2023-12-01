48bc6e5ff8e0f0ccbc75f2ebb71075b10fcd7487
bilal abdelkadir <bilalbinabdelkadir@gmail.com>
2023-12-01 22:06:47 +0300

    feat: added navbar

---
+++ client/src/app/destinations/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>Destination</div>;
+};
+
+export default page;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,6 @@
+import HeroSection from '@/components/HeroSection';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 
 export default function Home() {
-  return (
-    <MaxWidthWrapper>
-      <h1 className="text-4xl font-bold">Home</h1>
-    </MaxWidthWrapper>
-  );
+  return <HeroSection />;
 }
