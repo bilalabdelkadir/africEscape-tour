3b81af346f053dac685e7cd3a5ebb1d411c0ff78
bilal abdelkadir <bilalbinabdelkadir@gmail.com>
2023-12-06 17:32:06 +0300

    feat: finished moving to react

--- app/src/pages/Auth/signup-tourist/SignupTourist.tsx
+++ app/src/pages/Auth/signup-tourist/SignupTourist.tsx
@@ -1,7 +1,164 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  Form,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Routes } from '@/constants/routes';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { NavLink } from 'react-router-dom';
+import { z } from 'zod';
+
+const SingupSchema = z
+  .object({
+    firstName: z.string().min(1, { message: 'Firstname is required' }),
+    lastName: z.string().min(1, { message: 'Lastname is required' }),
+    email: z.string().min(1, { message: 'Email is required' }).email({
+      message: 'Must be a valid email',
+    }),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be atleast 6 characters' }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: 'Password must be atleast 6 characters' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: "Password don't match",
+  });
+
+type ISignupSchema = z.infer<typeof SingupSchema>;
 
 const SignupTourist = () => {
-  return <div>SignupTourist</div>;
+  const form = useForm<ISignupSchema>({
+    resolver: zodResolver(SingupSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  function onSubmit(values: ISignupSchema) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex justify-center items-center font-jost">
+      <Card className="w-[350px] md:w-[450px] shadow-lg mx-2 ">
+        <CardHeader>
+          <CardTitle>Create An Account</CardTitle>
+          <CardDescription>
+            Already have an account?{' '}
+            <NavLink to={Routes.TOURIST_LOGIN} className="text-blue-500">
+              Login here
+            </NavLink>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Joe" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Doe" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="emaple@mail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" {...field} />
+                    </FormControl>
+                    <FormMessage>
+                      {form.formState.errors.confirmPassword &&
+                        form.formState.errors.confirmPassword.message}
+                    </FormMessage>
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="submit"
+                className="rounded-3xl w-full mx-auto"
+                size={'lg'}
+                // disabled={isPending}
+              >
+                {/* {isPending && (
+                  <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
+                )} */}
+                Sign up
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default SignupTourist;
--- app/src/router.tsx
+++ app/src/router.tsx
@@ -3,6 +3,13 @@ import Layout from './pages/Layout/Layout';
 import Home from './pages/home/Home';
 import SignupTourist from './pages/Auth/signup-tourist/SignupTourist';
 import LoginTourist from './pages/Auth/login-tourist/LoginTourist';
+import Destinations from './pages/desitnations/Destinations';
+import Destination from './pages/desitnations/Destination';
+import AgencyProfile from './pages/Profiles/agency/AgencyProfile';
+import TouristProfile from './pages/Profiles/tourist/TouristProfile';
+import Blogs from './pages/blogs/Blogs';
+import Contact from './pages/contact/Contact';
+import { Routes } from './constants/routes';
 
 const router = createBrowserRouter([
   {
@@ -10,8 +17,14 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-      { path: '/tourist/signup', element: <SignupTourist /> },
-      { path: '/tourist/login', element: <LoginTourist /> },
+      { path: Routes.TOURIST_SIGN_UP, element: <SignupTourist /> },
+      { path: Routes.TOURIST_LOGIN, element: <LoginTourist /> },
+      { path: Routes.DESTINATIONS, element: <Destinations /> },
+      { path: Routes.DESTINATIONPAGE, element: <Destination /> },
+      { path: Routes.AGENCY_PROFILE, element: <AgencyProfile /> },
+      { path: Routes.TOURIST_PROFILE, element: <TouristProfile /> },
+      { path: Routes.CONTACT, element: <Contact /> },
+      { path: Routes.BLOGS, element: <Blogs /> },
     ],
   },
 ]);
