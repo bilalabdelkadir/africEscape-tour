d3899113ff2f71a5edac16ddc795d9846463c743
bilal abdelkadir <bilalbinabdelkadir@gmail.com>
2023-12-18 18:23:25 +0300

    feat: added tour service

--- api/src/tours/tours.module.ts
+++ api/src/tours/tours.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ToursService } from './tours.service';
 import { ToursController } from './tours.controller';
 import { FileUploadModule } from 'src/file-upload/file-upload.module';
+import { TagsService } from './tags/tags.service';
 
 @Module({
   imports: [FileUploadModule],
   controllers: [ToursController],
-  providers: [ToursService],
+  providers: [ToursService, TagsService],
 })
 export class ToursModule {}
