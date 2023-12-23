aee36950004503fd615ea9f5798a35f89701f90d
bilal abdelkadir <bilalbinabdelkadir@gmail.com>
2023-12-23 04:24:58 +0300

    geat: added tour list page

--- app/src/pages/Layout/Layout.tsx
+++ app/src/pages/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import { isUserLoggedIn, user, agency } from '@/global-state/user.globalstate';
 import { fetchUserProfile } from '@/hooks/queryHooks';
 import { useEffect } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import { LoaderIcon } from 'lucide-react';
 import { AccountType } from '@/types';
 const Layout = () => {
@@ -21,6 +22,7 @@ const Layout = () => {
     <>
       <Navbar />
       <ScrollRestoration />
+      <Toaster className="font-Jost text-lg" richColors duration={3000} />
       <main className="relative flex flex-col min-h-screen" id="main">
         {isLoading ? (
           <div className="flex justify-center items-center h-screen">
